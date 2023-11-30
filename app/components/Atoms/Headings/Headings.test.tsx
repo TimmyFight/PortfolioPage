00444b124971bf521297f0ae3c0e43728a5a8eb6
Adrian <adrian.ciolkiewicz@makimo.pl>
2023-11-30 13:19:41 +0100

    feat: Install Jest and React Testing Library and add Headings test

---
+++ app/components/Atoms/Headings/Headings.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Headings from "./Headings";
+import "@testing-library/jest-dom";
+
+describe("Headings", () => {
+  it("The Headings rendered with default properties", () => {
+    render(<Headings />);
+    const heading = screen.getByTestId("heading");
+    expect(heading.tagName).toBe("H1");
+    expect(heading.textContent).toBe("");
+  });
+  it("The Headings rendered with a specific properties", () => {
+    render(
+      <Headings level={3}>
+        <>Test H3</>
+      </Headings>
+    );
+    const heading = screen.getByTestId("heading");
+    expect(heading.tagName).toBe("H3");
+    expect(heading.textContent).toBe("Test H3");
+  });
+});
