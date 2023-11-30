88c32636e669109d2a8513e6f021f29422b68cb0
Adrian <adrian.ciolkiewicz@makimo.pl>
2023-11-30 22:19:47 +0100

    feat: Add test for MainHeader and Menu Molecules

---
+++ app/components/Molecules/MainHeader/MainHeader.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import MainHeader from "./MainHeader";
+import "@testing-library/jest-dom";
+
+describe("MainHeader", () => {
+  it("The MainHeader component is rendered correctly", () => {
+    render(<MainHeader />);
+    const mainHeader = screen.getByTestId("mainHeader");
+
+    expect(mainHeader).toBeInTheDocument();
+    expect(mainHeader.querySelector("h1")).toHaveClass("font-bold text-4xl");
+    expect(mainHeader.querySelector("h2")).toHaveClass("font-bold text-xl");
+    expect(mainHeader.querySelector("p")).toHaveClass("pt-2 text-emerald-700");
+  });
+});
---
+++ app/components/Molecules/Menu/Menu.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import Menu from "./Menu";
+import { MAIN_MENU_ITEMS } from "@/constants";
+import "@testing-library/jest-dom";
+
+describe("Menu", () => {
+  it("The Menu rendered with links according to imported constant", () => {
+    render(<Menu />);
+    const menu = screen.getByTestId("menu");
+    const menuLinks = menu.querySelectorAll("a");
+    menuLinks.forEach((link: HTMLElement) => {
+      const isLinkInConst = MAIN_MENU_ITEMS.find(
+        (menuItem) =>
+          menuItem.href === link.getAttribute("href") &&
+          menuItem.name === link.textContent
+      );
+
+      expect(isLinkInConst).toBeDefined();
+    });
+  });
+});
