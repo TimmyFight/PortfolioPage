3c8eed0941410526d4ddf88f4f86728b5db7838c
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-01-03 08:39:24 +0100

    fix: Change Link to a tag for anchor links, add smooth scroll and dalay for transition

--- app/components/Atoms/MenuItem/MenuItem.tsx
+++ app/components/Atoms/MenuItem/MenuItem.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useSelector, useDispatch } from "react-redux";
 import { setActiveLink } from "@/lib/services/activeLinkSlice";
 import { RootState } from "@/lib/services/StoreProvider";
@@ -17,12 +16,12 @@ const MenuItem = ({ item }: MenuItemProperties) => {
   const activeLink = useSelector((state: RootState) => state.activeLink);
   return (
     <li
-      className={`transition-all ${
+      className={`transition-all delay-150 ${
         activeLink === item.href
           ? "duration-300 ease-in lg:relative left-full text-lg"
           : "max-lg:hidden left-0"
       }`}>
-      <Link
+      <a
         data-testid="menuItem"
         href={item.href}
         className={`cursor-pointer font-bold underline-offset-4 hover:underline hover:text-emerald-500 ${
@@ -32,7 +31,7 @@ const MenuItem = ({ item }: MenuItemProperties) => {
         }`}
         onClick={() => dispatch(setActiveLink(`${item.href}`))}>
         {item.name}
-      </Link>
+      </a>
     </li>
   );
 };
